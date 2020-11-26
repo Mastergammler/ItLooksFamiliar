01fd3e8ec71fd5f853370465d29a9eb32282a637
mastergammler <raphael-hoppe@gmx.de>
2020-11-26 10:41:34 +0100

    Fixed the tooltip issue

--- Assets/Project/InventoryUI.cs
+++ Assets/Project/InventoryUI.cs
@@ -7,17 +7,20 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     //todo: initialize with dynamic inventory count
-    private Image[] mChildImages; 
+    private Image[] mChildImages;
+    private ItemTooltip[] mTooltips;
+
     //todo: fix direct player access
     public GameObject Player;
-    // Start is called before the first frame update
     private IInventory mInv;
     void Start()
     {
         mChildImages = new Image[8];
-        for(int i = 0; i < 8; i++)
+        mTooltips = new ItemTooltip[8];
+        for (int i = 0; i < 8; i++)
         {
             mChildImages[i] = transform.GetChild(i).GetChild(0).GetComponent<Image>();
+            mTooltips[i] = transform.GetChild(i).GetComponent<ItemTooltip>();
         }
 
         mInv = Player.GetComponent<IInventory>();
@@ -31,15 +34,16 @@ public class InventoryUI : MonoBehaviour
 
     private void UpdateInventoryUI(object sender, InventoryObject e)
     {
-        if(e.Item == null)
+        if (e.Item == null)
         {
             mChildImages[e.SlotNo].sprite = null;
-            mChildImages[e.SlotNo].color = new Color(0,0,0,0);
+            mChildImages[e.SlotNo].color = new Color(0, 0, 0, 0);
         }
         else
         {
             mChildImages[e.SlotNo].sprite = e.Item.Image;
-            mChildImages[e.SlotNo].color = new Color(1,1,1,1);
+            mChildImages[e.SlotNo].color = new Color(1, 1, 1, 1);
         }
+        mTooltips[e.SlotNo].UpdateTooltipMessage(e.Item);
     }
 }
---
+++ Assets/Project/ItemTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MgSq.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(ICollectable))]
+public class ItemTooltip : TooltipTrigger
+{
+    private CollectableSO mObjectValues;
+
+
+    public void UpdateTooltipMessage(CollectableSO newObject)
+    {
+        mObjectValues = newObject;
+    }
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (mObjectValues != null)
+        {
+            TooltipHeader = mObjectValues.Name;
+            TooltipContent = mObjectValues.Description;
+            base.OnPointerEnter(eventData);
+        }
+    }
+}
