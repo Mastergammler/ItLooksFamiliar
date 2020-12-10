9a0e4db0d057225d3d8cf6acf9020f65fe81fd49
mastergammler <raphael-hoppe@gmx.de>
2020-12-10 11:35:57 +0100

    Refactored screen shake script

--- Assets/02 Areas/Code/Effects/ScreenShake.cs
+++ Assets/02 Areas/Code/Effects/ScreenShake.cs
@@ -7,74 +7,94 @@ namespace ItLooksFamiliar.Effects
 {
     public class ScreenShake : MonoBehaviour
     {
+        //##################
+        //##    EDITOR    ##
+        //##################
+
         public float ScreenShakeAmplitude = 1.5f;
         public float FrequencyRamp = 10f;
         public float ShakeTime = 1f;
+
         //###############
         //##  MEMBERS  ##
         //###############
+
         private CinemachineVirtualCamera mCamera;
+        private CinemachineBasicMultiChannelPerlin mShakeParams;
         private Coroutine mCurrentRoutine;
 
         //################
         //##    MONO    ##
         //################
+            
         void Awake()
         {
             mCamera = GameObject.FindWithTag("MainCamera").GetComponentInChildren<CinemachineVirtualCamera>();
+            if(mCamera == null) Debug.LogError("Cinemachine virtual camera not found, but is required!");
+            mShakeParams = mCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            if(mShakeParams == null) Debug.LogError("Virtual camera is missing the noise component");
         }
 
-        private IEnumerator screenShakeWithAutoDisable(float delay = 0f)
-        {
-            yield return new WaitForSeconds(delay);
-            if(mCamera != null)
-            {
-                var noise = mCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-                noise.m_AmplitudeGain = ScreenShakeAmplitude;
-                noise.m_FrequencyGain = FrequencyRamp;
-                yield return new WaitForSeconds(ShakeTime);
-                noise.m_AmplitudeGain = 0f;
-                noise.m_FrequencyGain = 0f;
-            }
-            yield return null;
-        }
-        private IEnumerator screenShakeWithCustomTime(float shakeTime)
-        {       
-            if(mCamera != null)
-            {
-                var noise = mCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-                noise.m_AmplitudeGain = ScreenShakeAmplitude;
-                noise.m_FrequencyGain = FrequencyRamp;
-                yield return new WaitForSeconds(shakeTime);
-                noise.m_AmplitudeGain = 0f;
-                noise.m_FrequencyGain = 0f;
-            }
-            yield return null;
-
-        }
+        //#################
+        //##  INTERFACE  ##
+        //#################
 
         public void TimedExecution(float shakeTime)
         {
-            if(mCurrentRoutine != null) StopCoroutine(mCurrentRoutine);
-            mCurrentRoutine = StartCoroutine(screenShakeWithCustomTime(shakeTime));
+            executeShake(() => StartCoroutine(shakeCustomTime(shakeTime)));
         }
-        public void Execute()
+
+        public void ExecuteDefault()
         {
-            if(mCurrentRoutine != null)
-            {
-                StopCoroutine(mCurrentRoutine);
-            }
-            mCurrentRoutine = StartCoroutine(screenShakeWithAutoDisable());
+            executeShake(() => StartCoroutine(shakeAutoDisable()));
         }
 
         public void ExecuteWithDelay(float delay)
         {
-            if(mCurrentRoutine != null)
-            {
-                StopCoroutine(mCurrentRoutine);
-            }
-            mCurrentRoutine = StartCoroutine(screenShakeWithAutoDisable(delay));
+            executeShake(() => StartCoroutine(shakeAutoDisable(delay)));
         }
 
+        private delegate Coroutine ExecuteShake();
+        private void executeShake(ExecuteShake shake)
+        {
+            if(mCurrentRoutine != null) StopCoroutine(mCurrentRoutine);
+            mCurrentRoutine = shake.Invoke();
+        }
+
+        //###############
+        //##  METHODS  ##
+        //###############
+
+        // Noise params have to be reset, else camera will shake all the time
+        private IEnumerator shakeAutoDisable(float delay = 0f)
+        {
+            yield return new WaitForSeconds(delay);
+
+            setShakeParams();
+            yield return new WaitForSeconds(ShakeTime);
+
+            resetShakeParams(); 
+            yield return null;
+        }
+
+        private IEnumerator shakeCustomTime(float shakeTime)
+        {       
+            setShakeParams();
+            yield return new WaitForSeconds(shakeTime);
+
+            resetShakeParams();
+            yield return null;
+        }
+
+        private void setShakeParams()
+        {
+            mShakeParams.m_AmplitudeGain = ScreenShakeAmplitude;
+            mShakeParams.m_FrequencyGain = FrequencyRamp;
+        }
+        private void resetShakeParams()
+        {
+            mShakeParams.m_AmplitudeGain = 0f;
+            mShakeParams.m_FrequencyGain = 0f;
+        }
     }
 }
